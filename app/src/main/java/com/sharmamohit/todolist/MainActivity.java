0c47e81b27a7c3fee6d78cd5b8b6eb15f5d0d444
mohitsharma44 <mohitsharma44@gmail.com>
2017-08-02 22:14:49 -0400

    Edit the list

--- app/src/main/java/com/sharmamohit/todolist/MainActivity.java
+++ app/src/main/java/com/sharmamohit/todolist/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sharmamohit.todolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,10 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lvItems = (ListView) findViewById(R.id.lvItems);
         items = new ArrayList<>();
-        itemsAdapter = new ArrayAdapter(this,
+        itemsAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.enter_task);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
     }
 }
